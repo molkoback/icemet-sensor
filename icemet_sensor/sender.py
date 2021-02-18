9902aedd0feed83b4a859a1a51a6b834a53a407b
molko <molkoback@gmail.com>
2021-02-18 14:16:34 +0200

    fix sender reconnection

--- icemet_sensor/sender.py
+++ icemet_sensor/sender.py
@@ -27,21 +27,32 @@ class Sender:
 		self._client = aioftp.Client()
 		self._pool = concurrent.futures.ProcessPoolExecutor()
 	
+	async def _connected(self):
+		try:
+			await self._client.command("NOOP")
+			return (await self._client.parse_line())[0] == "200"
+		except:
+			return False
+	
 	async def _connect(self):
 		await self._client.connect(self.ctx.cfg.ftp.host, port=self.ctx.cfg.ftp.port)
 		await self._client.login(self.ctx.cfg.ftp.user, self.ctx.cfg.ftp.passwd)
 		logging.debug("Connected")
 	
-	async def _reconnect(self, n):
-		for i in range(1, n+1):
-			try:
-				logging.debug("Reconnect attempt {}/{}".format(i, n))
-				await self._connect()
-			except:
-				await asyncio.sleep(60)
-	
 	async def _cycle(self):
 		files = await self.ctx.loop.run_in_executor(self._pool, _find_files, self.ctx.cfg.save.dir)
+		if not files:
+			await asyncio.sleep(1.0)
+			return
+		
+		if not await self._connected():
+			try:
+				await self._connect()
+			except:
+				logging.error("FTP server offline")
+				await asyncio.sleep(1.0)
+				return
+		
 		for f in files:
 			if self.ctx.quit.is_set():
 				break
@@ -53,23 +64,18 @@ class Sender:
 				await self._client.upload(fn_in, self.ctx.cfg.ftp.tmp, write_into=True)
 				await self._client.rename(self.ctx.cfg.ftp.tmp, fn_out)
 			except:
-				await self._reconnect(3)
-				break
+				logging.error("Upload failed")
+				await asyncio.sleep(1.0)
+				return
 			
 			await self.ctx.loop.run_in_executor(None, os.remove, fn_in)
 			logging.debug("Sent {} ({:.2f} s)".format(f.name(), time.time()-t))
-		if not files:
-			await asyncio.sleep(1.0)
-	
-	async def _run(self):
-		logging.info("FTP server {}:{}".format(self.ctx.cfg.ftp.host, self.ctx.cfg.ftp.port))
-		await self._connect()
-		while not self.ctx.quit.is_set():
-			await self._cycle()
 	
 	async def run(self):
 		try:
-			await self._run()
+			logging.info("FTP server {}:{}".format(self.ctx.cfg.ftp.host, self.ctx.cfg.ftp.port))
+			while not self.ctx.quit.is_set():
+				await self._cycle()
 		except KeyboardInterrupt:
 			self.ctx.quit.set()
 		except Exception as e:

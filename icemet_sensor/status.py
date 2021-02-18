42b06b0a6c50a76197e7c626ed8f90bf05021d9e
molko <molkoback@gmail.com>
2021-02-18 16:31:22 +0200

    disable SSL if needed

--- icemet_sensor/status.py
+++ icemet_sensor/status.py
@@ -1,19 +1,33 @@
 import aiohttp
+import certifi
 
 import asyncio
 import logging
+import ssl
 import time
 
 class Status:
 	def __init__(self, ctx, **kwargs):
 		self.ctx = ctx
 		self.delay = kwargs.get("delay", 10 * 60)
+		self._ssl_ctx = None
+	
+	async def _create_ssl_context(self, session):
+		ssl_ctx = ssl.create_default_context(cafile=certifi.where())
+		try:
+			await session.post(self.ctx.cfg.status.url, ssl=ssl_ctx)
+			self._ssl_ctx = ctx
+		except:
+			self._ssl_ctx = False
 	
 	async def _send(self):
 		async with aiohttp.ClientSession() as session:
+			if self._ssl_ctx is None:
+				await self._create_ssl_context(session)
+			
 			# Login
 			auth = aiohttp.BasicAuth(self.ctx.cfg.status.user, self.ctx.cfg.status.passwd)
-			await session.post(self.ctx.cfg.status.url, auth=auth)
+			await session.post(self.ctx.cfg.status.url, auth=auth, ssl=self._ssl_ctx)
 			
 			# Send message
 			form = {
@@ -21,7 +35,7 @@ class Status:
 				"name": self.ctx.cfg.sensor.name,
 				"time": time.time()
 			}
-			async with session.post(self.ctx.cfg.status.url, data=form) as resp:
+			async with session.post(self.ctx.cfg.status.url, data=form, ssl=self._ssl_ctx) as resp:
 				delay = (await resp.json())["delay"]
 				logging.debug("Status message sent ({:.2f} s)".format(delay))
 	

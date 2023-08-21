261da88ad3e76c0bb465b1c6e78fb0af26785a1a
molko <molkoback@gmail.com>
2023-08-21 21:51:28 +0300

    release 2.2.0

--- icemet_sensor/__init__.py
+++ icemet_sensor/__init__.py
@@ -2,7 +2,7 @@ import asyncio
 import datetime
 import os
 
-version = "2.2.0-dev"
+version = "2.2.0"
 homedir = os.path.join(os.path.expanduser("~"), ".icemet")
 datadir =  os.path.join(os.path.dirname(__file__), "data")
 

2e08b2eca82aec62e2e60dade51c66acd0bafd9c
molko <molkoback@gmail.com>
2024-04-03 18:08:34 +0300

    more sophisticated plugin loading

--- icemet_sensor/plugins.py
+++ icemet_sensor/plugins.py
@@ -1,14 +1,18 @@
 import importlib
 import logging
-import sys
+import os
 
 class PluginContainer:
 	def __init__(self, plugins_path):
-		sys.path.append(plugins_path)
+		self.plugins_path = plugins_path
 		self._plugins = {}
 	
 	def load(self, name):
-		module = importlib.import_module(name)
+		path = os.path.join(self.plugins_path, name+".py")
+		spec = importlib.util.spec_from_file_location(name, path)
+		module = importlib.util.module_from_spec(spec)
+		spec.loader.exec_module(module)
+		
 		count = 0
 		for func_name in dir(module):
 			if func_name.startswith("on_"):
@@ -16,6 +20,7 @@ class PluginContainer:
 				if not func_name in self._plugins:
 					self._plugins[func_name] = []
 				self._plugins[func_name].append(getattr(module, func_name))
+		
 		logging.debug("Plugin '{}' with {} hooks".format(name, count))
 	
 	async def call(self, name, *args, **kwargs):
